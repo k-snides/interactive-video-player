f4637bfc3ee6c4761ea1b6ac86ee95740e5b2c90
Snide <snide78@hotmail.com>
2016-11-30 03:58:37 -0500

    current time and duration of video displaying

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-(function($) {
+// (function($) {
 
+window.onload = init;
+
+/**
+ * [init - Main function runs on page load]
+ */
+function init() {
 	// Global Variables
 	var video      = document.getElementById('theVideo');
 	var playPause  = document.getElementById('playPause');
 	var restart    = document.getElementById('restart');
 	var videoEnd   = document.getElementById('videoEnd');
-	
-	
+
+
+	/**
+	 * [displayVideoCurrentTime - displays video current time]
+	 */
+	function displayVideoCurrentTime() {
+		var videoCurrentTime = document.getElementById('videoCurrentTime');
+		var displayVideoTime = Math.floor(video.currentTime);
+		displayVideoTime = displayVideoTime < 10 ? '0' + displayVideoTime : displayVideoTime;
+		displayVideoTime = '00:' + displayVideoTime + ' /';
+		videoCurrentTime.textContent = displayVideoTime;
+	}
+
+
+	/**
+	 * [displayVideoLength - displays length of video]
+	 * @param  {[number]} vidLength [length of video]
+	 */
+	function displayVideoLength(vidLength) {
+		vidLength = Math.floor(vidLength);
+		// Display video duration
+		var displayLength = document.getElementById('vidLength');
+		vidLength = '00:' + vidLength;
+		displayLength.textContent = vidLength;
+	}
+
+
 	// Plays video and toggles play/pause button
 	playPause.onclick = function() {	
 		
@@ -21,21 +52,30 @@
 		    playPause.setAttribute('src', 'icons/play-icon.png');
 		}
 	};
-		
-		
+
+	// Get video length for duration display
+	var vidLength  = video.duration;
+	displayVideoLength(vidLength);
+
+	// Get video current time for display
+	video.ontimeupdate = function() {
+		displayVideoCurrentTime();
+	};
+	
+	// Skips to the beginning of the video
 	restart.addEventListener("click", function() {
 		video.currentTime = 0;
 	}, false);
 
+	// Skips to the end of the video
 	videoEnd.addEventListener("click", function() {
-		var getVideoDuration = video.duration;
-		video.currentTime = getVideoDuration;
-		console.log(getVideoDuration);
+		video.currentTime = vidLength;
 	}, false);
+}// end of init function
 
 
 		
 
 	
 	
-})(jQuery);
+// })(jQuery);
