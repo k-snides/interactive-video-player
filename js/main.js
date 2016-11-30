cee8309755249d85c0b40d410b6108aa6c502318
Snide <snide78@hotmail.com>
2016-11-30 07:09:02 -0500

    rewind, fastforward & volume controls implemented

--- js/main.js
+++ js/main.js
@@ -6,11 +6,33 @@ window.onload = init;
  * [init - Main function runs on page load]
  */
 function init() {
+	
 	// Global Variables
-	var video      = document.getElementById('theVideo');
-	var playPause  = document.getElementById('playPause');
-	var restart    = document.getElementById('restart');
-	var videoEnd   = document.getElementById('videoEnd');
+	var video         = document.getElementById('theVideo');
+	var playPause     = document.getElementById('playPause');
+	var restart       = document.getElementById('restart');
+	var videoEnd      = document.getElementById('videoEnd');
+	var rewind        = document.getElementById('rewind');
+	var forward       = document.getElementById('forward');
+	var volumeUp      = document.getElementById('volumeUp');
+	var volumeDown    = document.getElementById('volumeDown');
+
+	
+		// video.addEventListener("loadedmetadata", function() {
+		// 	var duration = video.duration;
+		// 	console.log(duration);
+		// }, false);	
+
+
+
+	// Get video duration
+	var vidLength  = video.duration;
+	vidLength = Math.floor(vidLength);
+
+	// Display video duration
+	var displayLength = document.getElementById('vidLength');
+	vidLength = '00:' + vidLength;
+	displayLength.textContent = vidLength;
 
 
 	/**
@@ -29,13 +51,13 @@ function init() {
 	 * [displayVideoLength - displays length of video]
 	 * @param  {[number]} vidLength [length of video]
 	 */
-	function displayVideoLength(vidLength) {
-		vidLength = Math.floor(vidLength);
-		// Display video duration
-		var displayLength = document.getElementById('vidLength');
-		vidLength = '00:' + vidLength;
-		displayLength.textContent = vidLength;
-	}
+	// function displayVideoLength(vidLength) {
+	// 	vidLength = Math.floor(vidLength);
+	// 	// Display video duration
+	// 	var displayLength = document.getElementById('vidLength');
+	// 	vidLength = '00:' + vidLength;
+	// 	displayLength.textContent = vidLength;
+	// }
 
 
 	// Plays video and toggles play/pause button
@@ -53,9 +75,13 @@ function init() {
 		}
 	};
 
+	// Set volume at half
+	video.volume = .5;
+	console.log(video.volume);
+
 	// Get video length for duration display
-	var vidLength  = video.duration;
-	displayVideoLength(vidLength);
+	// var vidLength  = video.duration;
+	// displayVideoLength(vidLength);
 
 	// Get video current time for display
 	video.ontimeupdate = function() {
@@ -71,6 +97,35 @@ function init() {
 	videoEnd.addEventListener("click", function() {
 		video.currentTime = vidLength;
 	}, false);
+
+	// Rewind 10 seconds
+	rewind.addEventListener("click", function() {
+		video.currentTime -= 10;
+	}, false);
+
+	// Fast forward 10 seconds
+	forward.addEventListener("click", function() {
+		video.currentTime += 10;
+	}, false);
+
+	// Turns volume up 10%
+	volumeUp.addEventListener("click", function() {
+		video.volume += 0.1;
+		if (video.volume >= 0.9) {
+			video.volume = 0.9;
+		}
+		console.log(video.volume);
+	}, false);
+
+	// Turns volumen down 10%
+	volumeDown.addEventListener("click", function() {
+		video.volume -= 0.1;
+		if (video.volume <= 0.1) {
+			video.volume = 0.1;
+		}
+		console.log(video.volume);
+	}, false);
+
 }// end of init function
 
 
