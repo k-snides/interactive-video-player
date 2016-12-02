43395d5deadc6c682042a484e73205d8fa2328ac
Snide <snide78@hotmail.com>
2016-12-02 04:11:21 -0500

    volume buttons configured to unmute

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function init() {
 	// Global Variables
 	var video         = document.getElementById('theVideo');
 	var playPause     = document.getElementById('playPause');
+	var muteUnmute    = document.getElementById('muteUnmute');
 	var restart       = document.getElementById('restart');
 	var videoEnd      = document.getElementById('videoEnd');
 	var rewind        = document.getElementById('rewind');
@@ -17,28 +18,89 @@ function init() {
 	var volumeUp      = document.getElementById('volumeUp');
 	var volumeDown    = document.getElementById('volumeDown');
 
+	// TODO: Get duration to show properly and not NaN
+	// video.addEventListener("loadedmetadata", function() {
+	// 	var duration = video.duration;
+	// 	console.log(duration);
+	// }, false);
 	
-		// video.addEventListener("loadedmetadata", function() {
-		// 	var duration = video.duration;
-		// 	console.log(duration);
-		// }, false);	
-
-
 
 	// Get video duration
-	var vidLength  = video.duration;
-	vidLength = Math.floor(vidLength);
+	// var vidLength  = video.duration;
+	// console.log(vidLength);
+	// vidLength = Math.floor(vidLength);
+	// console.log(vidLength);
 
-	// Display video duration
+
+	// Display vidLength
 	var displayLength = document.getElementById('vidLength');
+	var vidLength = video.duration;
+	console.log(vidLength);
+
+	vidLength = Math.floor(vidLength);
 	vidLength = '00:' + vidLength;
 	displayLength.textContent = vidLength;
+
+	console.log(vidLength);
+
+	// Get video length for duration display
+	// var vidLength  = video.duration;
+	// displayVideoLength(vidLength);
+
+	
+	/**
+	 * [playOrPause - Plays video and toggles play/pause button]
+	 * @return {[type]} [description]
+	 */
+	var playOrPause = function() {		
+		// if video is paused, play video and display pause icon
+		if (video.paused) {   
+		    video.play();
+		    playPause.setAttribute('src', 'icons/pause-icon.png');
+		    playPause.setAttribute('alt', 'Pause button');
+		}
+		// otherwise if video is playing, pause video and display play icon
+		else { 
+		    video.pause();
+		    playPause.setAttribute('src', 'icons/play-icon.png');
+		    playPause.setAttribute('alt', 'Play button');
+		}
+	};
+
+
+	/**
+	 * [muteOrUnmute - Mutes video and toggles mute/unmute button]
+	 */
+	var muteOrUnmute = function() {
+		// if video is muted, unmute video and display unmute icon
+		if (video.muted) {
+			video.muted = false;
+			muteUnmute.setAttribute('src', 'icons/volume-on-icon.png');
+			muteUnmute.setAttribute('alt', 'Volume on button');
+		}
+		// otherwise if video is unmuted, mute video and display mute icon
+		else {
+			video.muted = true;
+			muteUnmute.setAttribute('src', 'icons/volume-off-icon.png');
+			muteUnmute.setAttribute('alt', 'Volume off button');
+		}
+	};
+
+
+	/**
+	 * [unmuteWithVolumeButtons - unmutes audio with volume buttons]
+	 */
+	var unmuteWithVolumeButtons = function() {
+		// Calls muteOrUnmute() to unmute and get 'Volume off icon'
+		muteOrUnmute();
+		video.volume = video.volume;
+	};
 
 
 	/**
 	 * [displayVideoCurrentTime - displays video current time]
 	 */
-	function displayVideoCurrentTime() {
+	var displayVideoCurrentTime = function() {
 		var videoCurrentTime = document.getElementById('videoCurrentTime');
 		var displayVideoTime = Math.floor(video.currentTime);
 		displayVideoTime = displayVideoTime < 10 ? '0' + displayVideoTime : displayVideoTime;
@@ -60,28 +122,21 @@ function init() {
 	// }
 
 
-	// Plays video and toggles play/pause button
-	playPause.onclick = function() {	
-		
-		// if video is paused, play video and display pause icon
-		if (video.paused) {   
-		    video.play();
-		    playPause.setAttribute('src', 'icons/pause-icon.png');
-		}
-		// otherwise if video is playing, pause video and display play icon
-		else { 
-		    video.pause();
-		    playPause.setAttribute('src', 'icons/play-icon.png');
-		}
-	};
+	
+
+	playPause.addEventListener("click", function() {
+		playOrPause();
+	}, false);
+
+	muteUnmute.addEventListener("click", function() {
+		muteOrUnmute();
+	}, false);
 
 	// Set volume at half
 	video.volume = .5;
 	console.log(video.volume);
 
-	// Get video length for duration display
-	// var vidLength  = video.duration;
-	// displayVideoLength(vidLength);
+	
 
 	// Get video current time for display
 	video.ontimeupdate = function() {
@@ -95,7 +150,8 @@ function init() {
 
 	// Skips to the end of the video
 	videoEnd.addEventListener("click", function() {
-		video.currentTime = vidLength;
+		// video.currentTime = vidLength;
+		video.currentTime = video.duration;
 	}, false);
 
 	// Rewind 10 seconds
@@ -114,6 +170,9 @@ function init() {
 		if (video.volume >= 0.9) {
 			video.volume = 0.9;
 		}
+		if (video.muted) {
+			unmuteWithVolumeButtons();			
+		}
 		console.log(video.volume);
 	}, false);
 
@@ -122,6 +181,9 @@ function init() {
 		video.volume -= 0.1;
 		if (video.volume <= 0.1) {
 			video.volume = 0.1;
+		}
+		if (video.muted) {
+			unmuteWithVolumeButtons();
 		}
 		console.log(video.volume);
 	}, false);
